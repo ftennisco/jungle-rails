06174b0a376c5c7eae5a8c31318b7f060a68ea9d
ftennisco <f.tennisco@hotmail.com>
2023-11-08 17:49:18 -0800

    Implement an About Us page.

---
+++ app/controllers/about_controller.rb
@@ -0,0 +1,5 @@
+class AboutController < ApplicationController
+  def index
+    # Static content can be added here if needed
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,10 @@ Rails.application.routes.draw do
     resources :products, except: [:edit, :update, :show]
   end
 
+  get '/about', to: 'about#index', as: 'about'
+end
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -71,4 +75,4 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+
